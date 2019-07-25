ded41e8ca6e8cc607ab44ac1361b0370354162b7
Michał Misiarek <michalmisiarek@Michas-MBP.fritz.box>
2019-07-25 05:54:20 +0200

    add material ui and styled-components

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
+import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
+
+const MyH1 = styled.div`
+    color: tomato;
+`;
+
 function Home() {
-    return <div>Welcome to Meal Planner!</div>;
-  }
-  
-  export default Home;
+    return (<div>
+        <MyH1>
+            Welcome to Meal Planner!
+        </MyH1>
+        <Button>click</Button>
+    </div>);
+}
+
+export default Home;
